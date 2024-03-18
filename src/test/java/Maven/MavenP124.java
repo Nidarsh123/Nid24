29b18d4785cda50861a10c5da681a56c8a5f2036
Nidarsh123 <nidarshnidu003@gmail.com>
2024-03-18 17:14:26 +0530

    pushone

---
+++ src/test/java/Maven/MavenP124.java
@@ -0,0 +1,11 @@
+package Maven;
+
+import org.testng.annotations.Test;
+
+public class MavenP124 {
+
+	@Test
+	public void p2test() {
+		System.out.println("practice");
+	}
+}
---
+++ src/test/java/Maven/MavenP24.java
@@ -0,0 +1,12 @@
+package Maven;
+
+import org.testng.annotations.Test;
+
+public class MavenP24 {
+
+	@Test
+	public void P1test() {
+		System.out.println("hi");
+		System.out.println("hello");
+	}
+}
