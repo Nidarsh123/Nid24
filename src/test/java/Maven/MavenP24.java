3d87b366d7da789b03f63f10600a370efec8f332
Nidarsh123 <142539574+Nidarsh123@users.noreply.github.com>
2024-03-18 20:04:03 +0530

    Update MavenP24.java

--- src/test/java/Maven/MavenP24.java
+++ src/test/java/Maven/MavenP24.java
@@ -6,15 +6,13 @@ public class MavenP24 {
 
 	@Test
 	public void P1test() {
+		//pull
 		System.out.println("hi");
 		System.out.println("hello");
-<<<<<<< HEAD
 		//push
 		//pull123
 		//pull
-=======
 		//pull branch
 		//push again github
->>>>>>> firstbranch
 	}
 }
