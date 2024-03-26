7a8bc16fde98c384bfd85e0a9c417553cd398333
Nidarsh123 <nidarshnidu003@gmail.com>
2024-03-26 19:37:45 +0530

    push4

--- src/test/java/Maven1/MavenPractice.java
+++ src/test/java/Maven1/MavenPractice.java
@@ -6,7 +6,7 @@ public class MavenPractice {
 	@Test
 	public void test() {
 		System.out.println("smoke test");
-		//smoke test 2
+		//smoke test 26
 		//pull to eclipse
 		System.out.println("smoke");
 	}
